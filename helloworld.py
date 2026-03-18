77788fbd4ede3592a0bc6fd85e725be3bab8187b
nadjatiihonen <nadja.toroponina@gmail.com>
2026-03-18 22:19:29 +0200

    Remove redundant files from root

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand) :
-    help = "Prints Hello world"
-
-    def handle (self, *args, **kwargs) :
-        # we write the logic
-        self.stdout.write("hello world")
