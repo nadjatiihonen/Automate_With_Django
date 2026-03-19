2fe38c26c4f5ebad7495c519dd1e36a06e924f79
nadjatiihonen <nadja.toroponina@gmail.com>
2026-03-19 07:13:11 +0200

    Added command to import data from CSV file

---
+++ dataentry/management/commands/importdata.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand 
+from dataentry.models import Student
+import csv
+
+# Proposed command - python manage.py importdata file_path
+
+class Command(BaseCommand):
+    help = 'Import data from CSV file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file_path', type=str, help='Path to the CSV file')
+
+    def handle(self, *args, **kwargs):
+        # logic goes here
+        file_path = kwargs['file_path']
+        with open (file_path, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                Student.objects.create(**row)
+        self.stdout.write(self.style.SUCCESS('Data imported from CSV successefully'))
