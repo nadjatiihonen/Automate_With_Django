6454e4824e97c88c290a79f9ec717f1c91a03eb7
nadjatiihonen <nadja.toroponina@gmail.com>
2026-03-28 23:20:33 +0200

    Remove secret key from settings
    Removed the secret key for security reasons.

--- automate_with_django_main/settings.py
+++ automate_with_django_main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_g!d3_@1@=842hlr#j44u&p_ucvkphi_5+s%j5nc-h=gyyyg7m"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
